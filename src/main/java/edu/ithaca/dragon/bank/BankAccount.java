8bde3fbad052712b7a52167c320c4dc833058844
Sean McQuillan <mcquill99@gmail.com>
2020-01-26 17:02:33 -0500

    Added extra functionality to withdraw method

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -28,9 +28,16 @@ public class BankAccount {
 
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
+     * throws InsufficientFundsException if the amount is larger than the balance
+     * If balance is negative, do nothing
      */
-    public void withdraw (double amount)  throws InsufficientFundsException {
-        balance -= amount;
+    public void withdraw (double amount) throws InsufficientFundsException  {
+        if (balance >= amount && amount > 0) {
+            balance -= amount;
+        }
+        else if(balance < amount){
+            throw new InsufficientFundsException("Amount requested is more than in your account by " + (amount - balance));
+        }
 
     }
 
